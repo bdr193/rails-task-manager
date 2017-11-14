f324b70c5525676969104c8553836a723e565874
rabea bader <bdr193@gmail.com>
2017-11-14 11:45:19 +0100

    created controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get '/tasks'
+
+  get '/tasks/:id'
+
+  get '/tasks/new'
+
+  post '/tasks'
+
+  get '/tasks/:id/edit'
+
+  patch '/tasks/:id'
+
+  delete '/tasks/:id'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
